9ce027a0458ea6a36527657478c8604296ac2fb5
Sendeke, Kidus <kiduss@uw.edu>
2019-02-04 12:47:55 -0800

    Updates

---
+++ precache-manifest.c07b6431adecced5169067d0d8e8d4a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f50d72958ff51dc3f856",
+    "url": "/landing-page/static/css/main.c1da173b.chunk.css"
+  },
+  {
+    "revision": "f50d72958ff51dc3f856",
+    "url": "/landing-page/static/js/main.f50d7295.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/landing-page/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5e502b1b7d9d8dee4025",
+    "url": "/landing-page/static/js/runtime~main.5e502b1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/landing-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "13bcd2fd8d2192299b02c1aec4756bb8",
+    "url": "/landing-page/index.html"
+  }
+];
