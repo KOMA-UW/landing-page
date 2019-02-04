c01a498b6815b7804265967772f2110f1b6fef43
Sendeke, Kidus <kiduss@uw.edu>
2019-02-04 13:27:33 -0800

    skeleton done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LandingPage from './LandingPage';
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faIgloo } from '@fortawesome/free-solid-svg-icons'
 ReactDOM.render(<LandingPage />, document.getElementById('root'));
