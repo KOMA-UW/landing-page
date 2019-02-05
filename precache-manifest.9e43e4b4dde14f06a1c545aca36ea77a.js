1962ec647e31a09b3fce5f08b1a358742dd9e7b5
Sendeke, Kidus <kiduss@uw.edu>
2019-02-05 12:27:55 -0800

    Updates

--- precache-manifest.9e43e4b4dde14f06a1c545aca36ea77a.js
+++ precache-manifest.9e43e4b4dde14f06a1c545aca36ea77a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "82f41f560f7f64613723",
+    "revision": "fed06b402003a11e1328",
     "url": "/landing-page/static/css/main.f32229e1.chunk.css"
   },
   {
-    "revision": "82f41f560f7f64613723",
-    "url": "/landing-page/static/js/main.82f41f56.chunk.js"
+    "revision": "fed06b402003a11e1328",
+    "url": "/landing-page/static/js/main.fed06b40.chunk.js"
   },
   {
     "revision": "4a850e01e56550ee24e1",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/landing-page/static/js/runtime~main.5e502b1b.js"
   },
   {
-    "revision": "c013004eddfc6bbf666a1975f77041a8",
+    "revision": "c4c9c7bb03719012592461a5b91f094e",
     "url": "/landing-page/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/landing-page/precache-manifest.83de5e3c93e7f6ce03d89d0ecca805af.js"
+  "/landing-page/precache-manifest.9e43e4b4dde14f06a1c545aca36ea77a.js"
 );
 
 workbox.clientsClaim();
