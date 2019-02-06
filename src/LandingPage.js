9c2f5922d67bbbac0c5d7a3ef0b79ce79e682f44
Sendeke, Kidus <kiduss@uw.edu>
2019-02-05 20:18:51 -0800

    added nav, logos and css

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -3,7 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import './index.css';
-// import {Card, CardTitle, Row, Col, Navbar, NavItem, CardPanel} from 'react-materialize';
 
 
 const styles = theme => ({
@@ -21,90 +20,116 @@ function LandingPage(props) {
   const classes = props;
       return (
         <div className={classes.root}>
-          <nav>
-          <div className="nav-wrapper">
-            <h2 className="title">Grow Your Dough, GYD</h2>
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <a className="navbar-brand " href="#page-top">
+      {/* <img alt="" src={require("./assets/comma_logo.jpg")} /> */}
+         <i className="fas fa-hands"></i> 
+         AllHandsOnDeck</a>
+      <button className="navbar-toggler navbar-toggler-right collapsed" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <i className="fa fa-bars"></i>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link " href="#overview">Overview</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link " href="#team">Team</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link " href="#contact">Contact</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link " href="#sponsors">Sponsors</a>
+          </li>
+        </ul>
+      </div>
+  </nav>
+
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4">AHOD</h1>
+            <p className="lead">A web platform that connects students with 
+            their trusted friends and family to create a strong financial community to address student debt.</p>
           </div>
-        </nav>
-        <div className="container">
-          <section>
-            <div>
-              <h2 className="titles">What is this?</h2>
+        </div>
+
+          <div className="container">
+            {/* <section id="overview">
+              <div>
+                <h2 className="titles">WHAT IS IT?</h2>
+              </div>
               <div className="content">
                 <p>Content goes here..</p>
               </div>
-            </div>
-          </section>
-  
-          <section> 
-            
-            <div className="titles">
-              <h2>Meet the Team</h2>
-            </div>
-            <div className="row">
-              <div className="col-lg-2">
-                <img className="bd-placeholder-img rounded-circle" alt="" src="https://www.gravatar.com/avatar/329c17c44c22a447870f4a44dc058b4d?s=1000" width="100" height="100" focusable="false" role="img" aria-label="Placeholder: 140x140"/><title>Placeholder</title>
-                <h4>Kidus Sendeke</h4>
-                <p>Senior, Informatics</p>
-                <a href="mailto:kiduss@uw.edu">kiduss@uw.edu</a><br/>
-                <a href="https://www.linkedin.com/in/kidus-sendeke/"><i className="fab fa-linkedin"></i></a>
-              </div>
-              <div className="col-lg-2">
-                <img className="bd-placeholder-img rounded-circle" alt="" src="https://www.gravatar.com/avatar/50afbe369dcb2cd4ff4dee5c5e2efca1?s=1000" width="100" height="100" role="img" aria-label="Placeholder: 140x140"/><title>Placeholder</title>
-                <h4>Omar Azeemi</h4>
-                <p>Senior, Informatics</p>
-                <a href="mailto:oazeemi@gmail.com">oazeemi@gmail.com</a><br/>   
-                <a href="https://www.linkedin.com/in/omar-azeemi/"><i className="fab fa-linkedin"></i></a>
+            </section> */}
+    
+            <section id="team"> 
               
+              <div className="titles">
+                <h2>MEET THE TEAM</h2>
               </div>
-              <div className="col-lg-2">
-                <img className="bd-placeholder-img rounded-circle" alt="" src="https://www.gravatar.com/avatar/e2de7dff621ccfad3a32c5112984583d?s=1000" width="100" height="100" role="img" aria-label="Placeholder: 140x140"/><title>Placeholder</title>
-                <h4>Muatasim Qazi</h4>
-                <p>Senior, Informatics</p>
-                <a href="mailto:mqazi@uw.edu">mqazi@uw.edu</a><br/>
-                <a href="https://www.linkedin.com/in/muatasimqazi/"><i className="fab fa-linkedin"></i></a>
-          
+
+              <div className="row">
+                <div className="col-lg-3">
+                  <img className="bd-placeholder-img rounded-circle" alt="" src="https://www.gravatar.com/avatar/329c17c44c22a447870f4a44dc058b4d?s=1000" width="100" height="100" focusable="false" role="img" aria-label="Placeholder: 140x140"/><title>Placeholder</title>
+                  <h5>Kidus Sendeke</h5>
+                  <p>Senior, Informatics</p>
+                  <a href="mailto:kiduss@uw.edu">kiduss@uw.edu</a><br/>
+                  <a href="https://www.linkedin.com/in/kidus-sendeke/"><i className="fab fa-linkedin"></i></a>
+                </div>
+                <div className="col-lg-3">
+                  <img className="bd-placeholder-img rounded-circle" alt="" src="https://www.gravatar.com/avatar/50afbe369dcb2cd4ff4dee5c5e2efca1?s=1000" width="100" height="100" role="img" aria-label="Placeholder: 140x140"/><title>Placeholder</title>
+                  <h5>Omar Azeemi</h5>
+                  <p>Senior, Informatics</p>
+                  <a href="mailto:oazeemi@gmail.com">oazeemi@gmail.com</a><br/>   
+                  <a href="https://www.linkedin.com/in/omar-azeemi/"><i className="fab fa-linkedin"></i></a>
+                </div>
+                <div className="col-lg-3">
+                  <img className="bd-placeholder-img rounded-circle" alt="" src="https://www.gravatar.com/avatar/e2de7dff621ccfad3a32c5112984583d?s=1000" width="100" height="100" role="img" aria-label="Placeholder: 140x140"/><title>Placeholder</title>
+                  <h5>Muatasim Qazi</h5>
+                  <p>Senior, Informatics</p>
+                  <a href="mailto:mqazi@uw.edu">mqazi@uw.edu</a><br/>
+                  <a href="https://www.linkedin.com/in/muatasimqazi/"><i className="fab fa-linkedin"></i></a>
+                </div>
+                <div className="col-lg-3">
+                  <img className="bd-placeholder-img rounded-circle" alt="" src="https://www.gravatar.com/avatar/5d08ebbe27f488c85e54e869b4894926?s=1000" width="100" height="100" role="img" aria-label="Placeholder: 140x140"/><title>Placeholder</title>
+                  <h5>Alexis Lim</h5>
+                  <p>Senior, Informatics</p>
+                  <a href="mailto:alexisl@uw.edu">alexisl@uw.edu</a><br/>
+                  <a href="https://www.linkedin.com/in/calexlim/"><i className="fab fa-linkedin"></i></a>
+                </div>
               </div>
-              <div className="col-lg-2">
-                <img className="bd-placeholder-img rounded-circle" alt="" src="https://www.gravatar.com/avatar/3e705eefc666716240cbaa8d57dcd8a1?s=1000" width="100" height="100" role="img" aria-label="Placeholder: 140x140"/><title>Placeholder</title>
-                <h4>Alexis Lim</h4>
-                <p>Senior, Informatics</p>
-                <a href="mailto:alexisl@uw.edu">alexisl@uw.edu</a><br/>
-                <a href="https://www.linkedin.com/in/calexlim/"><i className="fab fa-linkedin"></i></a>
-              </div>
-               </div>
             </section>
 
 
-          <section>
-            <div>
-              <h2 className="titles">Contacts</h2>
-              <div className="content">
-                <p>Content goes here..</p>
+            <section>
+              <div>
+                <h2 className="titles">CONTACTS</h2>
+                <div className="content">
+                  <p>Content goes here..</p>
+                </div>
               </div>
-            </div>
-          </section>
+            </section>
 
           <section>   
-          <h2 className="titles">Sponsors</h2>      
-            <div className="content">
-            <div>
-              <img className="uw_logo"  alt="uw" src="https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/98/2014/10/07214216/Signature_Left_Purple_RGB.png"/>
-           </div>
-           <div>
-              <img className="ischool_logo"  alt="ischool"  src="./assets/iSchoolPrimary_RGB_Purple.jpg"/>
-              <img className="ischool_logo"  alt="ischool"  src="./assets/Logo_UW_.jpg"/>
-            </div>
-            </div>
-          </section>
-          
-            <footer className="page-footer fixed-bottom font-small bg-dark">  
-              <div className="footer-copyright text-center py-3">© 2019 Copyright  
-                <a href="https://github.com/orgs/KOMA-UW/dashboard"> KOMA Inc.</a>
+            <h2 className="titles">SPONSORS</h2>      
+              <div className="content">
+                <div className="logo_imgs">
+                    <img className="uw_logo"  alt="uw" src="https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/98/2014/10/07214216/Signature_Left_Purple_RGB.png"/>
+                    {/* <img className="uw_logo"  alt="uw" src="https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/98/2014/10/07214218/Signature_Left_Gold_RGB.png"/>  */}
+                    <img className="ischool_logo"  alt="ischool"  src={require('./assets/iSchoolPrimary_RGB_Purple.png')}/>
+                  </div>
               </div>
-          </footer>
-        </div>
-</div>
+            </section>
+            
+              <footer className="page-footer fixed-bottom font-small">  
+                <div className="footer-copyright text-center py-3"><p>© 2019 Copyright</p>  
+                  <a href="https://github.com/orgs/KOMA-UW/dashboard"> KOMA Inc.</a>
+                </div>
+              </footer>
+          </div>
+      </div>
   );
 }
 
